3bf6f1bf80d29181694917a6caf706443cf836ba
Harsh Kumar Pandey <harshkp20@iitk.ac.in>
2021-10-05 19:11:59 +0530

    Changes

--- project/scenes/physics/Char.gd
+++ project/scenes/physics/Char.gd
@@ -12,22 +12,22 @@ func _ready():
 
 func _physics_process(delta):
 	# Remove to Apply Gravity
-	# apply_gravity(delta)
+	apply_gravity(delta)
 	apply_motion(delta)
 	velocity = rb.move_and_slide(velocity)
 
 func apply_gravity(delta):
-	velocity.y+=9.8*gravityScale*delta*100.0
+	velocity.y+=9.8*gravityScale*delta*100.0 
 
 func apply_motion(delta):
 	var xDir=0.0
 	if Input.is_action_pressed("left"):
 		xDir = -1.0
 	elif Input.is_action_pressed("right"):
-		xDir = 1.0
+		xDir = 1.0 
 	if is_equal_approx(xDir,0.0):
 		velocity.x = lerp(velocity.x,0.0,friction)
 	else:
 		velocity.x += xDir*acceleration*delta
 	# Remove to Clamp Velocity
-	# velocity.x=clamp(velocity.x,-maxSpeed,maxSpeed)
+	velocity.x=clamp(velocity.x,-maxSpeed,maxSpeed)
